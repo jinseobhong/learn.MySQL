f9aae8288ef29c24e769a19fcadb19d8537c0e10
I Hyeon Hong <ihyeonhong@gmail.com>
2024-11-13 19:36:26 +0900

    Create 2024/11/13 files

---
+++ basic/select/distinct.sql
@@ -0,0 +1,12 @@
+-- Remove duplicates with distinct
+-- Please select only the department number.
+select distinct 
+	department_id 
+from employees
+
+-- When there are two or more columns, duplicates are removed when selected.
+select distinct
+	job_id,
+	department_id
+from
+	employees;
