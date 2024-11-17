2c30747930f06f69e959d43290be2376d23e740f
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:22:35 +0900

    Update Is_null.sql

--- sql/select/Is_null.sql
+++ sql/select/Is_null.sql
@@ -1,4 +1,5 @@
--- IS NULL : Select rows that cannot refer to data values
+-- IS NULL
+-- Select rows that cannot refer to data values
 -- Employees without commission
 -- NULL cannot be selected with equlas(=).
 use hr;
