2329405c1a1d9f356e452a2b93e3315736f806de
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:42:52 +0900

    Update case_conversion.sql

--- sql/select/case_conversion.sql
+++ sql/select/case_conversion.sql
@@ -1,18 +1,23 @@
 -- Case convension
--- upper : Convert all alphabets to uppercase
+
+-- upper
+-- Convert all alphabets to uppercase
 use hr;
 select
     first_name,
     upper(first_name) as uppercase_firstName
 from
     employees;
--- lower : Convert all alphabets to lowercase
+
+-- lower 
+-- Convert all alphabets to lowercase
 use hr;
 select
     first_name,
     lower(first_name) as lowercase_firstName
 from
     employees;
+
 -- Convert email to lowercase and select
 use hr;
 select
