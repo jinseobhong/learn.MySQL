f80f64015e2ca0c53f0de3378c7e9dbad5d9292a
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:56:37 +0900

    Update in.sql

--- sql/select/in.sql
+++ sql/select/in.sql
@@ -2,8 +2,9 @@
 -- Check if in contains one of all the values ​​contained in the parameter
 -- iN(a,b,...) : a or b or ..
 
--- When processing with or without using in
 -- Select people with job_Id : PU_CLERK or SH_CLERK or ST_CLERK
+
+-- When processing with or without using in
 use hr;
 select
 	*
@@ -13,6 +14,7 @@ where
 	job_id = 'PU_CLERK'
 	or job_id = 'SH_CLERK'
 	or job_id = 'ST_CLERK';
+
 -- When processing using in
 use hr;
 select
@@ -21,8 +23,8 @@ from
 	employees
 where
 	job_id in ('PU_CLERK', 'SH_CLERK', 'ST_CLERK');
--- Employees with supervisors with employee numbers 100, 101, and 102
--- Subordinates of numbers 101, 101, and 102
+
+-- Employees with supervisors with employee numbers 100, 101, and 102(Subordinates of numbers 101, 101, and 102)
 use hr;
 select
 	*
