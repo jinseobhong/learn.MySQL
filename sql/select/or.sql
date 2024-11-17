4686efc52def0302380787c56ab671a443e58b64
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 15:10:13 +0900

    Update or.sql

--- sql/select/or.sql
+++ sql/select/or.sql
@@ -1,5 +1,8 @@
-use hr;
+-- or
+-- Returns at least one value that is true
+
 -- Select employees with department numbers of 30 or AC_ACCOUNT positions
+use hr;
 select
 	*
 from
@@ -8,10 +11,11 @@ where
 	department_id = 30
 	or job_id = 'AC_ACCOUNT';
 -- Select employees whose department number is 80 or whose position is ST_MAN
+use hr;
 select
 	*
 from
 	employees
 where
 	department_id = 80
-	or job_id = 'ST_MAN';   
+	or job_id = 'ST_MAN';
