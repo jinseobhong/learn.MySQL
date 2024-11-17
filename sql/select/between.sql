4c9ce3e33c459658072c410c45a53d3e5807219a
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:30:45 +0900

    Update between.sql

--- sql/select/between.sql
+++ sql/select/between.sql
@@ -2,6 +2,8 @@
 -- Values ​​between A and B
 -- between A and B
 -- Exception occurs if B is greater than A
+
+-- Select employees with salary between 5000 and 10000$
 use hr;
 select 
 	*
@@ -9,6 +11,7 @@ from
 	employees
 where
 	salary between 5000 and 10000;
+
 -- Select employees with salary between 3000 and 4000$
 use hr;
 select
