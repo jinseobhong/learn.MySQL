165c2d8eb8fd9db039329b38de7ff2fe0bba8ddf
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 15:13:34 +0900

    Update show.sql

--- sql/select/show.sql
+++ sql/select/show.sql
@@ -1,6 +1,9 @@
-use hr;
+-- show
+-- This command is used when you want to view various information, such as a database list or table list.
+
 -- Shows what databases exist
 show databases;
+
 -- Check the status of the table (database currently being used)
 show table status;
 show tables;
