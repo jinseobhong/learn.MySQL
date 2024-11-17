ccfa4c7fe00c3ac9c661e635101a9980afdc0ae4
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:32:29 +0900

    Update condition_operator.sql

--- sql/select/condition_operator.sql
+++ sql/select/condition_operator.sql
@@ -1,5 +1,6 @@
 -- Condition operator
 -- Comparing numbers
+
 -- Select people with annual salary (salary * 12) over 100000
 use hr;
 select
@@ -8,6 +9,7 @@ from
 	employees
 where
 	(salary * 12) >= 100000;
+
 -- Select people with a monthly salary of 15,000 or more
 use hr;
 select
@@ -16,8 +18,10 @@ from
 	employees
 where
 	salary >= 15000;
+
 -- Compare the size of characters
 -- In the dictionary, the later the value appears in alphabetical order, the larger the value.
+
 -- Select employees whose names are the same as or follow U
 use hr;
 select
@@ -26,6 +30,7 @@ from
 	employees
 where
 	first_name >= 'U';
+
 -- Select employees whose last name comes before G
 use hr;
 select
@@ -37,6 +42,7 @@ where
 
 -- Date data can also be compared.
 -- The further in the future the date is, the larger the number. Tomorrow is greater than today, and today is more valuable than yesterday.
+
 -- Select people whose hire date (hire_date) is after 2008
 use hr;
 select
