fe750e1fe69d9858b8ebbf9cbd258bd173e7a4c0
IHyeon Hong <ihyeonhong0427@gmail.com>
2024-11-17 14:24:03 +0900

    Update distinct.sql

--- sql/select/distinct.sql
+++ sql/select/distinct.sql
@@ -1,11 +1,12 @@
-use hr;
+-- Distinct
 -- Remove duplicates with distinct
 -- Please select only the department number.
+use hr;
 select distinct 
 	department_id 
 from employees
-
 -- When there are two or more columns, duplicates are removed when selected.
+use hr;	
 select distinct
 	job_id,
 	department_id
